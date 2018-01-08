09588eb8773d9e9e1d927a3eadfbf315d2154e7b
vndr <jv.vndr@gmail.com>
2018-01-08 07:12:33 +0000

    standard and python cannary pipelines added

---
+++ src/org/unomee/pythonPipeline.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/groovy
+package org.unomee;
+
+pythonPipeline(pipelineDefinition) {
+    // Create a globally accessible variable that makes
+    // the YAML pipeline definition available to all scripts
+    pd = pipelineDefinition
+}
+
+def executePipeline() {
+    node {
+        if (runTests) {
+            stage('Run Tests') {
+                sh pd.testCommand
+            }
+        }
+
+        if (deployUponTestSuccess) {
+            stage('Deploy') {
+                sh "path/to/a/deploy/bash/script.sh ${pd.deploymentEnvironment}"
+            }
+        }
+    }
+}
+
+return this
---
+++ src/org/unomee/stdPipeline.groovy
@@ -0,0 +1,27 @@
+# /src/org/unomee/stdPipeline.groovy
+#!/bin/groovy
+package org.unomee;
+
+def execute() {
+
+    node {
+
+        stage('Initialize') {
+            checkout scm
+            echo 'Loading pipeline definition'
+            Yaml parser = new Yaml()
+            Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yml').text)
+        }
+
+        switch(pipelineDefinition.pipelineType) {
+            case 'python':
+                // Instantiate and execute a Python pipeline
+                new pythonPipeline(pipelineDefinition).executePipeline()
+            case 'nodejs':
+                // Instantiate and execute a NodeJS pipeline
+                new nodeJSPipeline(pipelineDefinition).executePipeline()
+        }
+
+    }
+
+}
